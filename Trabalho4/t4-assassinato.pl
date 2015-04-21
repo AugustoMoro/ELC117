848d4a6f1feef599b8b8f041a8dc327940a6c476
AugustoMoro <morovaldebuia@gmail.com>
2015-04-21 11:21:05 -0300

    2

--- Trabalho4/t4-assassinato.pl
+++ Trabalho4/t4-assassinato.pl
@@ -1,43 +1,43 @@
-pedro(segunda, santa maria).
-pedro(terca, santa maria).
-pedro(quarta, porto alegre).
-pedro(quinta, santa maria).
-pedro(sexta, apartamento).
-caren(segunda, porto alegre).
-caren(terca, porto alegre).
-caren(quarta, porto alegre).
-caren(quinta, santa maria).
-caren(sexta, apartamento).
-henrique(segunda, apartamento).
-henrique(terca, porto alegre).
-henrique(quarta, apartamento).
-henrique(quinta, apartamento).
-henrique(sexta, apartamento).
-bia(segunda, apartamento).
-bia(terca, porto alegre).
-bia(quarta, porto alegre).
-bia(quinta, santa maria).
-bia(sexta, apartamento).
-adriano(segunda, apartamento).
-adriano(terca, apartamento).
-adriano(quarta, santa maria).
-adriano(quinta, apartamento).
-adriano(sexta, apartamento).
-alice(segunda, apartamento).
-alice(terca, porto alegre).
-alice(quarta, porto alegre).
-alice(quinta, apartamento).
-alice(sexta, apartamento).
-bernardo(segunda, santa, maria).
-bernardo(terca, santa maria).
-bernardo(quarta, porto alegre).
-bernardo(quinta, santa maria).
-bernardo(sexta, apartamento).
-maria(segunda, apartamento).
-maria(terca, santa maria).
-maria(quarta, santa maria).
-maria(quinta, santa maria).
-maria(sexta, apartamento).
+pedro(segunda,santamaria).
+pedro(terca,santamaria).
+pedro(quarta,portoalegre).
+pedro(quinta,santamaria).
+pedro(sexta,apartamento).
+caren(segunda,portoalegre).
+caren(terca,portoalegre).
+caren(quarta,portoalegre).
+caren(quinta,santamaria).
+caren(sexta,apartamento).
+henrique(segunda,apartamento).
+henrique(terca,portoalegre).
+henrique(quarta,apartamento).
+henrique(quinta,apartamento).
+henrique(sexta,apartamento).
+bia(segunda,apartamento).
+bia(terca,portoalegre).
+bia(quarta,portoalegre).
+bia(quinta,santamaria).
+bia(sexta,apartamento).
+adriano(segunda,apartamento).
+adriano(terca,apartamento).
+adriano(quarta,santamaria).
+adriano(quinta,apartamento).
+adriano(sexta,apartamento).
+alice(segunda,apartamento).
+alice(terca,portoalegre).
+alice(quarta,portoalegre).
+alice(quinta,apartamento).
+alice(sexta,apartamento).
+bernardo(segunda,santamaria).
+bernardo(terca,santamaria).
+bernardo(quarta,portoalegre).
+bernardo(quinta,santamaria).
+bernardo(sexta,apartamento).
+maria(segunda,apartamento).
+maria(terca,santamaria).
+maria(quarta,santamaria).
+maria(quinta,santamaria).
+maria(sexta,apartamento).
 
 pobre(bernardo).
 pobre(bia).
@@ -52,12 +52,16 @@ rico(adriano).
 insano(maria).
 insano(adriano).
 
-relacionamento(anita, bernardo).
-relacionamento(bernardo, caren).
-relacionamento(anita, pedro).
-relacionamento(pedro, alice).
-relacionamento(alice, henrique).
-relacionamento(henrique, maria).
-relacionamento(adriano, caren).
+casal(anita,bernardo).
+casal(bernardo,caren).
+casal(anita,pedro).
+casal(pedro,alice).
+casal(alice,henrique).
+casal(henrique,maria).
+casal(adriano,caren).
 
 vitima(anita).
+
+namorou(X,Y) :- casal(X,Y) , casal(Y,X).
+ciumes(X,Y) :- namorou(Z,X) , namorou(Y,Z).
+
