d0b6aea79df61f485b053106c4b7881cbff61a40
AugustoMoro <morovaldebuia@gmail.com>
2015-04-21 10:23:10 -0300

    1

---
+++ Trabalho4/t4-assassinato.pl
@@ -0,0 +1,63 @@
+pedro(segunda, santa maria).
+pedro(terca, santa maria).
+pedro(quarta, porto alegre).
+pedro(quinta, santa maria).
+pedro(sexta, apartamento).
+caren(segunda, porto alegre).
+caren(terca, porto alegre).
+caren(quarta, porto alegre).
+caren(quinta, santa maria).
+caren(sexta, apartamento).
+henrique(segunda, apartamento).
+henrique(terca, porto alegre).
+henrique(quarta, apartamento).
+henrique(quinta, apartamento).
+henrique(sexta, apartamento).
+bia(segunda, apartamento).
+bia(terca, porto alegre).
+bia(quarta, porto alegre).
+bia(quinta, santa maria).
+bia(sexta, apartamento).
+adriano(segunda, apartamento).
+adriano(terca, apartamento).
+adriano(quarta, santa maria).
+adriano(quinta, apartamento).
+adriano(sexta, apartamento).
+alice(segunda, apartamento).
+alice(terca, porto alegre).
+alice(quarta, porto alegre).
+alice(quinta, apartamento).
+alice(sexta, apartamento).
+bernardo(segunda, santa, maria).
+bernardo(terca, santa maria).
+bernardo(quarta, porto alegre).
+bernardo(quinta, santa maria).
+bernardo(sexta, apartamento).
+maria(segunda, apartamento).
+maria(terca, santa maria).
+maria(quarta, santa maria).
+maria(quinta, santa maria).
+maria(sexta, apartamento).
+
+pobre(bernardo).
+pobre(bia).
+pobre(pedro).
+pobre(maria).
+
+rico(caren).
+rico(alice).
+rico(henrique).
+rico(adriano).
+
+insano(maria).
+insano(adriano).
+
+relacionamento(anita, bernardo).
+relacionamento(bernardo, caren).
+relacionamento(anita, pedro).
+relacionamento(pedro, alice).
+relacionamento(alice, henrique).
+relacionamento(henrique, maria).
+relacionamento(adriano, caren).
+
+vitima(anita).
